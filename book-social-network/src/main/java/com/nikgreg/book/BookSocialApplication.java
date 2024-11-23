49787753061783c1352c4e105d12e9218587224c
Nicolas Gregori <nikgreg99@gmail.com>
2024-11-23 11:26:19 +0100

    Done project scaffolding

---
+++ book-social-network/src/main/java/com/nikgreg/book/BookSocialApplication.java
@@ -0,0 +1,13 @@
+package com.nikgreg.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSocialApplication.class, args);
+	}
+
+}
---
+++ book-social-network/src/test/java/com/nikgreg/book/BookSocialAoiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikgreg.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSocialAoiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

dfd2aecfd10589efaa61878eb980548f746dab77
Nicolas Gregori <nikgreg99@gmail.com>
2024-11-25 23:21:15 +0100

    Integrate auth domain and JWT filter chain support

---
+++ book-social-network/src/main/java/com/nikgreg/book/security/UserDetailServiceImpl.java
@@ -0,0 +1,23 @@
+package com.nikgreg.book.security;
+
+import com.nikgreg.book.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
---
+++ book-social-network/src/main/java/com/nikgreg/book/user/Token.java
@@ -0,0 +1,31 @@
+package com.nikgreg.book.user;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiredAt;
+    private LocalDateTime validateAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+
+}
